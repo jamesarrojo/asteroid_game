1c3d2a1aeec205350e0dc36ade89749981aa32f1
jamesarrojo <jamesarrojo.01@gmail.com>
2024-08-27 16:29:45 +0800

    Ch3-2:Collisions

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,6 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def have_collided(self, target):
+        return self.radius + target.radius >= self.position.distance_to(target.position)
--- main.py
+++ main.py
@@ -3,40 +3,49 @@ from constants import *
 import player # we could do this like this `from player import Player`, so that...
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+import sys
 
 def main():
   pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-  
   clock = pygame.time.Clock()
+  
   dt = 0
   playerInstance = player.Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS) # ...we just do this like this `player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)`
   
   updatable = pygame.sprite.Group()
   drawable = pygame.sprite.Group()
+  asteroids = pygame.sprite.Group()
 
   updatable.add(playerInstance)
   drawable.add(playerInstance)
   
-  asteroids = pygame.sprite.Group()
   Asteroid.containers = (asteroids, updatable, drawable)
   AsteroidField.containers = updatable
 
   asteroid_field = AsteroidField()
+  
   while True:
      for event in pygame.event.get():
         if event.type == pygame.QUIT:
            return
         
-     screen.fill("black")
-     dt = clock.tick(60) / 1000
-
      for updatableObj in updatable:
         updatableObj.update(dt)
+
+     for asteroid in asteroids:
+        if playerInstance.have_collided(asteroid) == True:
+           print('Game over!')
+           sys.exit()
+
+     screen.fill("black")
+
      for drawableObj in drawable:
         drawableObj.draw(screen)
 
      pygame.display.flip()
+
+     dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
